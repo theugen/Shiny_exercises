40593cb7855648b1346fdd191c11510e3bada5ad
Eugenio Thieme <eugen@thieme.it>
2017-03-01 15:38:42 +0100

    Fatto altri due esercizi con shiny

--- First_app/ui.R
+++ First_app/ui.R
@@ -1,6 +1,7 @@
 library(shiny)
 shinyUI(fluidPage(titlePanel(h1(strong('Shiny App'))), 
                   sidebarLayout(sidebarPanel(h1('Menu')), 
-                                mainPanel(h2('Main'))
+                                mainPanel(h2('Main'),
+                                          p('This famous (Fisher’s or Anderson’s)', a('iris', href = 'http://stat.ethz.ch/R-manual/R-devel/library/datasets/html/iris.html'), 'data set gives the measurements in centimeters of the variables sepal length and width and petal length and width, respectively, for 50 flowers from each of 3 species of iris. The species are Iris setosa, versicolor, and virginica.'))
                                 , position = 'left')))
 #runApp('First_app')
