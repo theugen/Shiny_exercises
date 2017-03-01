68754e0efb4f40fa9890674bbd911b7ea650bcdd
Eugenio Thieme <eugen@thieme.it>
2017-03-01 20:17:10 +0100

    -Ended first app (Iris)
    -Begun second app on buttons
    
    Exercises here:
    https://www.r-bloggers.com/building-shiny-app-exercises-part-2/7

--- First_app/ui.R
+++ First_app/ui.R
@@ -1,8 +1,8 @@
 library(shiny)
 shinyUI(fluidPage(titlePanel(h1(strong('Shiny App'))), 
-                  sidebarLayout(sidebarPanel(h1('Menu'), img(src='petal.png',
-                                                               width =200, height=250)), 
-                                mainPanel(h2('Main'),
+                  sidebarLayout(sidebarPanel(h1('Menu')), 
+                                mainPanel(h2('Main'), br(), br(),
+                                          img(src='petal.png', height=150, width=200),
                                           p('This famous (Fisher’s or Anderson’s)',
                                             a('iris', href = 'http://stat.ethz.ch/R-manual/R-devel/library/datasets/html/iris.html'),
                                             'data set gives the measurements in centimeters of the variables sepal length and width
