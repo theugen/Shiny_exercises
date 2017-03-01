55408bed87a6ffa28b55ddca38f7468aaaa3543a
Eugenio Thieme <eugen@thieme.it>
2017-03-01 17:23:44 +0100

    End of part one, here part two: https://www.r-bloggers.com/building-shiny-app-exercises-part-2/

--- First_app/ui.R
+++ First_app/ui.R
@@ -1,7 +1,14 @@
 library(shiny)
 shinyUI(fluidPage(titlePanel(h1(strong('Shiny App'))), 
-                  sidebarLayout(sidebarPanel(h1('Menu')), 
+                  sidebarLayout(sidebarPanel(h1('Menu'), img(src='petal.png',
+                                                               width =200, height=250)), 
                                 mainPanel(h2('Main'),
-                                          p('This famous (Fisher’s or Anderson’s)', a('iris', href = 'http://stat.ethz.ch/R-manual/R-devel/library/datasets/html/iris.html'), 'data set gives the measurements in centimeters of the variables sepal length and width and petal length and width, respectively, for 50 flowers from each of 3 species of iris. The species are Iris setosa, versicolor, and virginica.'))
+                                          p('This famous (Fisher’s or Anderson’s)',
+                                            a('iris', href = 'http://stat.ethz.ch/R-manual/R-devel/library/datasets/html/iris.html'),
+                                            'data set gives the measurements in centimeters of the variables sepal length and width
+                                            and petal length and width, respectively, for 50 flowers from each of 3
+                                            species of iris. The species are Iris',
+strong('setosa'), ',', strong('versicolor'), ', and ', strong('virginica'),'.'),
+                                          br(), h2('Analysis'))
                                 , position = 'left')))
 #runApp('First_app')
