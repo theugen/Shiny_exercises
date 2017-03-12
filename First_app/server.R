5a8f62f734c815b388c36c54b5cefa8c0d0b2e59
Eugenio Thieme <eugen@thieme.it>
2017-03-12 08:22:14 +0100

    Ended part 4 of Shiny app exercises. Here link to part 5: https://www.r-bloggers.com/building-shiny-app-exercises-part-5/

--- First_app/server.R
+++ First_app/server.R
@@ -1,1 +1,3 @@
-shinyServer(function(input,output){})
+shinyServer(function(input,output){
+  output$Table <- renderDataTable(iris, options = list(lengthMenu = list(c(10, 20, 30, -1), c('10','20','30','ALL')), pageLength=10))
+})
